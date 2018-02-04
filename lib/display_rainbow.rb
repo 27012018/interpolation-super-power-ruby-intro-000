493a83755f02d812473c1f5d81c5842cedcbae3a
Nana O <nancila.grace@gmail.com>
2018-02-04 17:00:49 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,11 @@
 # Write your #display_rainbow method here
-def display_rainbow
+def display_rainbow(colours)
+  colors=['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+  puts colors[1]
+  puts colors[2]
+  puts colors[3]
+  puts colors[4]
+  puts colors[5]
+  puts colors[6]
+  puts colors[7]
 end
